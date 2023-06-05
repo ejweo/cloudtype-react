a65a1b6d4b587d30032e729e1cd2a8f959232fc3
jhj <jhyunjin1994@gmail.com>
2023-06-06 08:04:24 +0900

    url change

--- src/services/WaitlistService.js
+++ src/services/WaitlistService.js
@@ -3,23 +3,23 @@ import axios from "axios";
 class WaitlistService{
 
     getAllGuests() {
-        return axios.get(process.env.REACT_APP_WAITLIST_API_URL)
+        return axios.get(process.env.REACT_APP_API_URL + "/api/guests")
     }
 
     createGuest(guest) {
-        return axios.post(process.env.REACT_APP_WAITLIST_API_URL, guest)
+        return axios.post(process.env.REACT_APP_API_URL + "/api/guests", guest)
     }
 
     getGuestById(guestId) {
-        return axios.get(process.env.REACT_APP_WAITLIST_API_URL + "/" + guestId)
+        return axios.get(process.env.REACT_APP_API_URL + "/api/guests/" + guestId)
     }
 
     updateGuest(guestId, guest) {
-        return axios.put(process.env.REACT_APP_WAITLIST_API_URL + "/" + guestId, guest)
+        return axios.put(process.env.REACT_APP_API_URL + "/api/guests/" + guestId, guest)
     }
 
     deleteGuest(guestId) {
-        return axios.delete(process.env.REACT_APP_WAITLIST_API_URL + "/" + guestId)
+        return axios.delete(process.env.REACT_APP_API_URL + "/api/guests/" + guestId)
     }
 }
 
