a6c196c0d0a5765ada9865736a2f66f16e7c7da0
jhj <jhyunjin1994@gmail.com>
2023-08-22 23:05:02 +0900

    사이드 메뉴 버튼 class 변경

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -9,20 +9,12 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  //Dropdown,
   Button,
 } from "reactstrap";
-//import user1 from "../assets/images/users/user1.jpg";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = React.useState(false);
   const [sideOpen, setSideOpen] = React.useState(true);
-  //const [dropdownOpen, setDropdownOpen] = React.useState(false);
 
-  //const toggle = () => setDropdownOpen((prevState) => !prevState);
-  const Handletoggle = () => {
-    setIsOpen(!isOpen);
-  };
   const showMobilemenu = () => {
     setSideOpen(!sideOpen);
 
@@ -30,23 +22,14 @@ const Header = () => {
   };
   return (
     <Navbar color="dark" dark expand="md">
-      <div className="d-flex align-items-center">
-        <Button
-          color="dark"
-          className = "d-lg-block"
-          onClick={() => showMobilemenu()}
-        >
-        <i className="bi bi-list"></i>
-        </Button>
-      </div>
       <div className="hstack gap-2">
         <Button
           color="dark"
           size="sm"
-          className="d-sm-block d-md-none"
-          onClick={Handletoggle}
+          className="d-sm-block d-md-block"
+          onClick={showMobilemenu}
         >
-          {isOpen ? (
+          {sideOpen ? (
             <i className="bi bi-x"></i>
           ) : (
             <i className="bi bi-three-dots-vertical"></i>
@@ -54,7 +37,7 @@ const Header = () => {
         </Button>
       </div>
 
-      <Collapse navbar isOpen={isOpen}>
+      <Collapse navbar sideOpen={sideOpen}>
         <Nav className="me-auto" navbar>
           <NavItem>
             <Link to="/starter" className="nav-link">
@@ -74,25 +57,6 @@ const Header = () => {
             </DropdownMenu>
           </UncontrolledDropdown>
         </Nav>
-        {/* <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-          <DropdownToggle color="dark">
-            <img
-              src={user1}
-              alt="profile"
-              className="rounded-circle"
-              width="30"
-            ></img>
-          </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem header>Info</DropdownItem>
-            <DropdownItem>My Account</DropdownItem>
-            <DropdownItem>Edit Profile</DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem>My Balance</DropdownItem>
-            <DropdownItem>Inbox</DropdownItem>
-            <DropdownItem>Logout</DropdownItem>
-          </DropdownMenu>
-        </Dropdown> */}
       </Collapse>
     </Navbar>
   );
